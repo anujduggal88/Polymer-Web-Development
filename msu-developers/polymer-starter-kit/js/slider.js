6776cc772409b6bec536e8d320554a8facc5b7f3
anujduggal88 <er.anujduggal@gmail.com>
2016-02-19 02:53:26 +0530

    WORKSHOP [STUDENT_YATRA]: MSU/Gujarat
    Created 'msu-developers' directory for MSU Participants.
    
    Added 'resource' directory for Polymer resources.
    
    Signed-off-by: anujduggal88 <er.anujduggal@gmail.com>

---
+++ msu-developers/polymer-starter-kit/js/slider.js
@@ -0,0 +1,16 @@
+
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+var sliderInt = 1;
+var sliderNext = 2;
+
+
+$(document).ready(function () {
+    "use strict";
+
+    $("#slider > img#1;").fadeIn(300);
+
+
+
+});
