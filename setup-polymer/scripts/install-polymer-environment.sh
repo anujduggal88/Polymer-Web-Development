db8f3709442bb37c60813b24eaadebdb40d59afd
Duggal, Anuj <anuj.duggal@intel.com>
2016-03-03 18:33:06 +0530

    Modified install-polymer-environment.sh file with latest updates
    Signed-off-by: Anuj Duggal <er.anujduggal@gmail.com>

--- setup-polymer/scripts/install-polymer-environment.sh
+++ setup-polymer/scripts/install-polymer-environment.sh
@@ -15,16 +15,16 @@
 #         NOTES:  ---
 #        AUTHOR: ANUJ DUGGAL
 #       LICENCE: Copyright (c) 2015, ANUJ DUGGAL
-#       COMPANY: 
+#       COMPANY: Intel, GDG Bangalore 
 #       CREATED: Sunday 19 April 2015 11:50:05  IST IST
-#      REVISION:  ---
+#      REVISION: Wednesday 3 Mar 2016 IST
 #===============================================================================
 
-set -o nounset                              # Treat unset variables as an error
+# INPUT NEEDED:
+POLYMER=~/polymer
 
 # DIRECTORIES:
 HOME=~/
-POLYMER=~/polymer1234
 ERROR_FILE="error_logs.txt"
 ERROR_LOGS=$POLYMER/$ERROR_FILE
 GDGPOLYMER=~/GDGPolymer
@@ -57,48 +57,89 @@ print_log() {
 	echo $(date "+%H:%M:%S") $1 '|' $2 | tee -a $LOG
 }
 
+
+# COLOR OUTPUT:
+NORMAL=$(tput sgr0)
+GREEN=$(tput setaf 2; tput bold)
+YELLOW=$(tput setaf 3)
+RED=$(tput setaf 1)
+ 
+red() {
+	echo "$RED$*$NORMAL"
+}
+ 
+green() {
+	echo "$GREEN$*$NORMAL"
+}
+ 
+yellow() {
+	echo "$YELLOW$*$NORMAL"
+}
+
+
 # SETTING UP DIRECTORY STRUCTURE:
 setup_dir(){
 
 	print_log "INFO" "Setting up Directory structure for Polymer.."
+	sleep 2
 
 	mkdir $POLYMER
 	if [ $? -ne 0 ]
 	then
-		print_log "ERROR" "Failed to create polymer Directory. Aborting further installation."
-		echo "ERROR | Failed to create polymer Directory. Aborting further installation." >> $ERROR_LOGS
+		# print_log "ERROR" "Failed to create polymer Directory. Aborting further installation."
+		# red "ERROR | Failed to create polymer Directory. Aborting further installation." >> $ERROR_LOGS
+		red "ERROR | Failed to create polymer Directory. Aborting further installation." >> $ERROR_LOGS
+
 		exit 1
 	else
-		print_ilog "INFO" "Polymer Directory created successully."
+		# print_log "INFO" "Polymer Directory created successully."
+		green "SUCCESS | Polymer Directory created successully."
 
 		# CREATE CUSTOM TEMPLATE FOLDER:
+		print_log "INFO" "Setting up Custom Template.."
+		sleep 2
+		
 		cd $POLYMER
 		mkdir $CUSTOM_ELEMENTS
 		if [ $? -ne 0 ]
 		then
-			print_log "ERROR" "Failed to create Custom Element folder."
-			echo "ERROR | Failed to create Custom Element folder." >> $ERROR_LOGS
+			# print_log "ERROR" "Failed to create Custom Element folder."
+			# red "ERROR | Failed to create Custom Element folder." >> $ERROR_LOGS
+			red "ERROR | Failed to create Custom Element folder."
 		else
-			print_log "INFO" "Custom Element folder created successully."
+			# print_log "INFO" "Custom Element folder created successully."
+			green "SUCCESS | Custom Element folder created successully."
+
+			# INITIATE CUSTOM TEMPLATE FOLDER:
+			print_log "INFO" "Initiating Custom Template directory.."
+			sleep 2
 
 			cp $CUSTOM_ELEMENTS_TEMPLATE $CUSTOM_ELEMENTS
 			if [ $? -ne 0 ]
 			then
-				print_log "ERROR" "Failed to copy custom-element."
-				echo "ERROR | Failed to copy custom-element." >> $ERROR_LOGS
+				# print_log "ERROR" "Failed to copy custom-element."
+				# red "ERROR | Failed to copy custom-element." >> $ERROR_LOGS
+				red "ERROR | Failed to initiate Custom Element directory."
 			else
-				print_log "INFO" "Custom Element template copied successfully."
+				# print_log "INFO" "Custom Element template copied successfully."
+				green "SUCCESS | Custom Element template directorty initiated successfully."
 			fi
 		fi
 
 		# COPY index.html from packages folder:
+		print_log "INFO" "Setting up Index File.."
+		sleep 2
+
 		cp $INDEX_FILE_TEMPLATE $POLYMER
 		if [ $? -ne 0 ]
 		then
-			print_log "ERROR" "Failed to copy index.html."
-			echo "ERROR | Failed to copy index.html." >> $ERROR_LOGS
+			# print_log "ERROR" "Failed to copy index.html."
+			# red "ERROR | Failed to copy index.html." >> $ERROR_LOGS
+			red "ERROR | Failed to initiate index.html."
 		else
-			print_log "INFO" "index.html template copied successfully."
+			# print_log "INFO" "index.html template copied successfully."
+			green "SUCCESS | index.html template initiated successfully."
+
 		fi
 	fi
 
@@ -109,18 +150,24 @@ setup_dir(){
 install_packages(){
 
 	print_log "INFO" "Installing Requierd Packages.."
+	sleep 2
 
 	# INSTALL NODEJS:
 	# UNSTAR THE PACKAGE:
+	print_log "INFO" "Setting up Node JS for Polymer.."
+	sleep 2
+	
 	cd $PACKAGES
 	$UNTAR_NODEJS
 	if [ $? -ne 0 ]
 	then
-		print_log "ERROR" "Failed to untar nodejs package. Aborting further installation."
-		echo "ERROR | Failed to untar nodejs package. Aborting further installation." >> $ERROR_LOGS
+		# print_log "ERROR" "Failed to untar nodejs package. Aborting further installation."
+		# red "ERROR | Failed to untar nodejs package. Aborting further installation." >> $ERROR_LOGS
+		red "ERROR | Failed to untar nodejs package. Aborting further installation."
 		exit 1
 	else
-		print_log "INFO" "nodejs package is un-tarred successully."
+		# print_log "INFO" "nodejs package is un-tarred successully."
+		green "SUCCESS | nodejs package is installed successully."
 	fi
 
 
@@ -128,25 +175,35 @@ install_packages(){
 	cd $POLYMER
 
 	#INSTALL GIT
+	print_log "INFO" "Installing GIT.."
+	sleep 2
+
 	$INSTALL_GIT
 	if [ $? -ne 0 ]
 	then
-		print_log "ERROR" "Failed to install Git. Aborting further installation."
-		echo "ERROR | Failed to install Git. Aborting further installation." >> $ERROR_LOGS
+		# print_log "ERROR" "Failed to install Git. Aborting further installation."
+		# red "ERROR | Failed to install Git. Aborting further installation." >> $ERROR_LOGS
+		red "ERROR | Failed to install Git. Aborting further installation."
 		exit 1
 	else
-		print_log "INFO" "Git installed successully."
+		# print_log "INFO" "Git installed successully."
+		green "SUCCESS | Git installed successully."
 	fi
 
 	# INSTALL BOWER:
+	print_log "INFO" "Installing Bower for Polymer.."
+	sleep 2
+
 	$INSTALL_BOWER
 	if [ $? -ne 0 ]
 	then
-		print_log "ERROR" "Failed to install Bower. Aborting further installation."
-		echo "ERROR | Failed to install Bower. Aborting further installation." >> $ERROR_LOGS
+		# print_log "ERROR" "Failed to install Bower. Aborting further installation."
+		# red "ERROR | Failed to install Bower. Aborting further installation." >> $ERROR_LOGS
+		red "ERROR | Failed to install Bower. Aborting further installation."
 		exit 1
 	else
-		print_log "INFO" "Bower installed successully."
+		# print_log "INFO" "Bower installed successully."
+		green "SUCCESS | Bower installed successully."
 	fi
 }
 
@@ -158,16 +215,24 @@ clone_polymer(){
 	cd $POLYMER
 
 	# INSTALL BOWER:
+	print_log "INFO" "initiating Bower.."
+	sleep 2
+
 	$INITIATE_BOWER
 
 	# INITIALIZE POLYMER REPO:
+	print_log "INFO" "Installing Polymer repository.."
+	sleep 2
+
 	$INSTALL_POLYMER_REPOSITORY
 	if [ $? -ne 0 ]
 	then
-		print_log "ERROR" "Failed to clone Polymer repository. Try again."
-		echo "ERROR | Failed to clone Polymer repository. Try again." >> $ERROR_LOGS
+		# print_log "ERROR" "Failed to clone Polymer repository. Try again."
+		# red "ERROR | Failed to clone Polymer repository. Try again." >> $ERROR_LOGS
+		red "ERROR | Failed to clone Polymer repository. Try again."
 	else
-		print_log "INFO" "Polymer repository initialized successfully."
+		# print_log "INFO" "Polymer repository initialized successfully."
+		green "SUCCESS | Polymer repository initialized successfully."
 	fi
 
 	# UPDATE BOWER:
@@ -186,7 +251,8 @@ setup_environment(){
 		# SETUP POLYMER DIRECTORY:
 		setup_dir
 	else
-		print_log "WARNING" "POLYMER Directory already exists. Aborting Installation. Delete already existing Polymer folder for fresh installation."
+		# print_log "WARNING" "POLYMER Directory already exists. Aborting Installation. Delete already existing Polymer folder for fresh installation."
+		yellow "WARNING | POLYMER Directory already exists. Aborting Installation. Delete already existing Polymer folder for fresh installation."
 		exit 1
 	fi
 
